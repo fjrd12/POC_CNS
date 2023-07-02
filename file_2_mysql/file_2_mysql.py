90a285d6950abddb48b5554154347cb8c9e0c0fb
Francisco Jhonathan Rodríguez Diaz <fjrd12@gmail.com>
2023-07-02 12:30:30 -0600

    Transform and validate version 0

--- file_2_mysql/file_2_mysql.py
+++ file_2_mysql/file_2_mysql.py
@@ -25,12 +25,13 @@ import logging
 import shutil
 import sys
 import tempfile
-import time
+#import time
+from datetime import datetime
 from pprint import pprint
 import csv
 import pendulum
-from colorama import Back, Fore, Style
-from airflow.models.xcom import XCom
+#from colorama import Back, Fore, Style
+#from airflow.models.xcom import XCom
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.python import ExternalPythonOperator, PythonVirtualenvOperator
@@ -71,113 +72,28 @@ with DAG(
     ingest_file_task = ingest_file()
     #[END _ingest file]
 
-    # [START howto_operator_python_render_sql]
-    #@task(task_id="log_sql_query", templates_dict={"query": "sql/sample.sql"}, templates_exts=[".sql"])
-    #def log_sql(**kwargs):
-    #    logging.info("Python task decorator query: %s", str(kwargs["templates_dict"]["query"]))
+    # [START validate_data]
+    @task(task_id="validate_data")
+    def validate_data(ds=None, **kwargs):
+        records = kwargs['ti'].xcom_pull(key='records')
+        for row in records:
+            log.info(row)
+            #apply_some_rules
+    validate_data_task = validate_data()
+    #[END validate_data]
 
-    #log_the_sql = log_sql()
-    # [END howto_operator_python_render_sql]
+    # [START transform data]
+    @task(task_id="transform_data")
+    def transform_data(ds=None, **kwargs):
+        records = kwargs['ti'].xcom_pull(key='records')
+        for row in records:
+            row[13] = datetime.today().strftime('%Y-%m-%d')
+        kwargs['ti'].xcom_push(key='records', value=records)
+    transform_data_task =transform_data()
+    #[END transform_data]
 
-    # [START howto_operator_python_kwargs]
-    # Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
-    #for i in range(5):
-
-    #    @task(task_id=f"sleep_for_{i}")
-    #    def my_sleeping_function(random_base):
-    #        """This is a function that will run within the DAG execution"""
-    #        time.sleep(random_base)
-
-    #    sleeping_task = my_sleeping_function(random_base=float(i) / 10)
-
-        #run_this >> log_the_sql >> sleeping_task
-    # [END howto_operator_python_kwargs]
-
-    if not shutil.which("virtualenv"):
-        log.warning("The virtalenv_python example task requires virtualenv, please install it.")
-    else:
-        # [START howto_operator_python_venv]
-        #@task.virtualenv(
-        #    task_id="virtualenv_python", requirements=["colorama==0.4.0"], system_site_packages=False
-        #)
-        #def callable_virtualenv():
-        #    """
-        #    Example function that will be performed in a virtual environment.
-
-        #    Importing at the module level ensures that it will not attempt to import the
-        #    library before it is installed.
-        #    """
-        #    from time import sleep
-
-        #    from colorama import Back, Fore, Style
-
-        #    print(Fore.RED + "some red text")
-        #    print(Back.GREEN + "and with a green background")
-        #    print(Style.DIM + "and in dim text")
-        #    print(Style.RESET_ALL)
-        #    for _ in range(4):
-        #        print(Style.DIM + "Please wait...", flush=True)
-        #        sleep(1)
-        #    print("Finished")
-
-        #virtualenv_task = callable_virtualenv()
-        # [END howto_operator_python_venv]
-
-        #sleeping_task >> virtualenv_task >> sleeping_task
-        # [START howto_operator_external_python]
-        #@task.external_python(task_id="external_python", python=PATH_TO_PYTHON_BINARY)
-        #def callable_external_python():
-        #    """
-        #    Example function that will be performed in a virtual environment.
-
-        #    Importing at the module level ensures that it will not attempt to import the
-        #   library before it is installed.
-        #    """
-        #    import sys
-        #    from time import sleep
-
-        #    print(f"Running task via {sys.executable}")
-        #    print("Sleeping")
-        #    for _ in range(4):
-        #        print("Please wait...", flush=True)
-        #        sleep(1)
-        #    print("Finished")
-
-        #external_python_task = callable_external_python()
-        # [END howto_operator_external_python]
-
-        # [START validate_data]
-        validate_data = ExternalPythonOperator(
-            task_id="validate_data",
-            python=PATH_TO_PYTHON_BINARY,
-            python_callable=x,
-        )
-        # [END validate_data]
-
-        # [START transform_data]
-        transform_data = ExternalPythonOperator(
-            task_id="transform_data",
-            python=PATH_TO_PYTHON_BINARY,
-            python_callable=x,
-        )
-        # [END transform_data]
-
-        # [START load_data]
-        load_data = ExternalPythonOperator(
-            task_id="load_data",                                                                                                                                         # [START howto_operator_python_venv_classic]
-            python=PATH_TO_PYTHON_BINARY,                                                                                                                                     #virtual_classic = PythonVirtualenvOperator(
-            python_callable=x,                                                                                                                                                #    task_id="virtualenv_classic",
-        )                                                                                                                                                                     #    requirements="colorama==0.4.0",
-        # [END load_data]
-
-
-        #    python_callable=x,
-        #)
-        # [END howto_operator_python_venv_classic]
-
-        #run_this >> external_classic >> external_python_task >> virtual_classic
-
-        ingest_file_task >> validate_data >> transform_data >> load_data
+    ingest_file_task >> validate_data_task >> transform_data_task
+    #>> load_data
 
 
 
